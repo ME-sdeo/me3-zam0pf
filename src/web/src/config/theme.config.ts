ccdc42397d1f84430ef308a66dfeb9d5d441c92d
ME-sdeo <160361169+ME-sdeo@users.noreply.github.com>
2025-01-21 10:10:05 -0600

    BugFix: Automated bug fixes for src/web/src/config/theme.config.ts

--- src/web/src/config/theme.config.ts
+++ src/web/src/config/theme.config.ts
@@ -174,6 +174,17 @@ export const themeConfig = {
     focusRingWidth: 3,
     highContrastSupport: true,
   },
+  transitions: {
+    duration: {
+      shortest: 150,
+      shorter: 200,
+      short: 250,
+      standard: 300,
+      complex: 375,
+      enteringScreen: 225,
+      leavingScreen: 195,
+    },
+  },
 };
 
 export default getThemeConfig;
